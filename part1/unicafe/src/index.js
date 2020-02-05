3e428161b86320631cd5ad7e12f96d87c97ad118
Jacob Biehler <biehlerj@gmail.com>
2020-02-04 16:11:05 -0800

    Adding code for exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = ({ good, neutral, bad }) => (
+  <div>
+    <div>{`good ${good}`}</div>
+    <div>{`neutral ${neutral}`}</div>
+    <div>{`bad ${bad}`}</div>
+  </div>
+)
+const Title = ({ title }) => (
+  <h1>{title}</h1>
+)
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <Title title="give feedback" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Title title="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
