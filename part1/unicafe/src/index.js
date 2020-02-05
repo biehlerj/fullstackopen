e932e6ee23bf40359648351062343384b6d6ac03
Jacob Biehler <biehlerj@gmail.com>
2020-02-04 16:26:42 -0800

    Adding code for exercise 1.9 (skipped 1.8 as I did that exercise by default)

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -14,16 +14,21 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = all > 0 ? (goodWeight + badWeight) / all : 'N/A'
   const positive = all > 0 ? `${good / all * 100}%` : 'N/A'
 
-  return (
-    <div>
-      <div>{`good ${good}`}</div>
-      <div>{`neutral ${neutral}`}</div>
-      <div>{`bad ${bad}`}</div>
-      <div>{`all ${all}`}</div>
-      <div>{`average ${average}`}</div>
-      <div>{`positive ${positive}`}</div>
-    </div>
-  )
+  if (all > 0) {
+    return (
+      <div>
+        <div>{`good ${good}`}</div>
+        <div>{`neutral ${neutral}`}</div>
+        <div>{`bad ${bad}`}</div>
+        <div>{`all ${all}`}</div>
+        <div>{`average ${average}`}</div>
+        <div>{`positive ${positive}`}</div>
+      </div>
+    )
+
+  } else {
+    return <div>{'No feedback given'}</div>
+  }
 }
 
 const Title = ({ title }) => (
